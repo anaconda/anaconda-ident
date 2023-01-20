be703c656ee9205f8761572569e50c0db6ac3fe6
Michael C. Grant <mgrant@anaconda.com>
2023-01-20 11:00:20 -0600

    fix windows

--- conda.recipe/bld.bat
+++ conda.recipe/bld.bat
@@ -1,5 +1,5 @@
 setlocal EnableDelayedExpansion
 %PREFIX%\python.exe -m pip install --no-deps --ignore-installed -vv .
-if "%CLIENT_TOKEN%" NEQ "" echo %CLIENT_TOKEN% > %SP_DIR%\conda_indent\client_token
+if "%CLIENT_TOKEN%" NEQ "" echo %CLIENT_TOKEN% > %SP_DIR%\conda_ident\client_token
 if not exist %PREFIX%\etc\conda\activate.d mkdir %PREFIX%\etc\conda\activate.d
 copy %RECIPE_DIR%\post-link.bat %PREFIX%\etc\conda\activate.d\%PKG_NAME%_activate.bat
