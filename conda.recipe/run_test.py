3a5540537e0d8f6646d8c94f77ba40ca8f3fcb8e
Michael C. Grant <mgrant@anaconda.com>
2023-01-21 10:13:24 -0600

    more fixes to this

--- conda.recipe/run_test.py
+++ conda.recipe/run_test.py
@@ -50,18 +50,19 @@ for flag in flags:
     print('{:{w1}} {:{w2}} ?? user-agent'.format('client_token', 'fields', w1=max_param, w2=max_field))
     print('{} {} -- ----------'.format('-' * max_param, '-' * max_field))
     for param, test_fields in test_patterns:
-        test_org = param.rsplit(':', 1)[-1]
         if test_fields and is_enabled:
             test_re = [c + '/[^ ]+' for c in test_fields]
             test_re_ua = '^.* ' + ' '.join(test_re) + '$'
         else:
             test_re_ua = '^((?! [cso]/).)*$'
+        print('{:{w1}} {:{w2}} '.format(param, test_fields, w1=max_param, w2=max_field), end='')
         os.environ['CONDA_CLIENT_TOKEN'] = param
         value = subprocess.check_output(['conda', 'info'])
         user_agent = next(v for v in value.decode('utf-8').splitlines() if 'user-agent' in v)
         user_agent = user_agent.split(' : ', 1)[-1]
-        failed = (not re.match(test_re_ua, user_agent) or
-                  is_enabled and 'o' in test_fields and ' o/' + test_org not in user_agent)
+        failed = not re.match(test_re_ua, user_agent)
+        if not failed and is_enabled and 'o' in test_fields:
+            failed = ' o/' + param.rsplit(':', 1)[-1] not in user_agent
         print('XX' if failed else 'OK', user_agent + '|')
         nfailed += failed
 
