c7e2d4e31942737afad23fe62c9c10108f29d2dc
Michael C. Grant <mgrant@anaconda.com>
2023-03-14 17:13:50 -0500

    embed repo url and token

--- conda.recipe/post-link.sh
+++ conda.recipe/post-link.sh
@@ -1,2 +1,2 @@
 pfx=${CONDA_PREFIX:-${PREFIX:-}}
-"$pfx/bin/python" -m conda_ident.install --enable >>"${pfx}/.messages.txt" 2>&1
+"$pfx/bin/python" -m conda_ident.install --enable --quiet >>"${pfx}/.messages.txt" 2>&1
