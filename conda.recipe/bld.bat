d763aeed43e32c5304d0c9b50af2282c4b725d85
Michael C. Grant <mgrant@anaconda.com>
2023-03-16 11:29:03 -0500

    fix winders

--- conda.recipe/bld.bat
+++ conda.recipe/bld.bat
@@ -1,5 +1,5 @@
 setlocal EnableDelayedExpansion
 %PREFIX%\python.exe -m pip install --no-deps --ignore-installed -vv .
-%PREFIX%\python.exe -m conda_ident.install --config "%CLIENT_TOKEN%" --default-channel "%DEFAULT_CHANNELS%" --channel-alias "%CHANNEL_ALIAS%" --token "%REPO_TOKEN%" --set-condarc --ignore-missing
+%PREFIX%\python.exe -m conda_ident.install --config "%CONFIG_STRING%" --default-channel "%DEFAULT_CHANNELS%" --channel-alias "%CHANNEL_ALIAS%" --token "%REPO_TOKEN%" --set-condarc --ignore-missing
 if not exist %PREFIX%\etc\conda\activate.d mkdir %PREFIX%\etc\conda\activate.d
 copy %RECIPE_DIR%\post-link.bat %PREFIX%\etc\conda\activate.d\%PKG_NAME%_activate.bat
