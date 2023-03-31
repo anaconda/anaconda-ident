1e50081c7725ed66be16dc155ff2f675e2c1d03f
Michael C. Grant <mgrant@anaconda.com>
2023-03-31 18:43:28 -0500

    fix key tests on winders

--- tests/test_keys.sh
+++ tests/test_keys.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
+source ${CONDA_PREFIX}/etc/profile.d/conda.sh
 SCRIPTDIR=$(cd $(dirname $BASH_SOURCE[0]) && pwd)
 MYTMPDIR="$(mktemp -d)"
 trap 'rm -rf -- "$MYTMPDIR"' EXIT
-source "$CONDA_PREFIX/etc/profile.d/conda.sh"
 channels=$(conda config --show channels | grep -v ^channels | sed 's@ *- *@@' | paste -s -d ',' -)
 export CONDA_CHANNELS="local,$channels"
 export KEY_VERSION=$(date +%Y%m%d)
