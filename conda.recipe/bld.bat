1409e49646fdfe7e16be9afd57a6e56a527a9a89
Michael C. Grant <mgrant@anaconda.com>
2023-10-04 07:18:26 -0500

    fix activate/post-link

--- conda.recipe/bld.bat
+++ conda.recipe/bld.bat
@@ -1,8 +1,11 @@
 setlocal EnableDelayedExpansion
 %PREFIX%\python.exe -m pip install --no-deps --ignore-installed -vv .
 if not exist %PREFIX%\etc\conda\activate.d mkdir %PREFIX%\etc\conda\activate.d
-if not exist %PREFIX%\python-scripts mkdir %PREFIX%\python-scripts
-copy scripts\activae.bat %PREFIX%\etc\conda\activate.d\%PKG_NAME%_activate.bat
+copy scripts\activate.bat %PREFIX%\etc\conda\activate.d\%PKG_NAME%_activate.bat
 copy scripts\activate.sh %PREFIX%\etc\conda\activate.d\%PKG_NAME%_activate.sh
-copy scripts\post-link.sh %PREFIX%\python-scripts\.%PKG_NAME%-post-link.sh
-copy scripts\pre-unlink.sh %PREFIX%\etc\conda\activate.d\.%PKG_NAME%-pre-unlink.sh
+if not exist %PREFIX%\Scripts mkdir %PREFIX%\Scripts
+copy scripts\post-link.bat %PREFIX%\Scripts\.%PKG_NAME%-post-link.bat
+copy scripts\pre-unlink.bat %PREFIX%\Scripts\.%PKG_NAME%-pre-unlink.bat
+if not exist %PREFIX%\bin mkdir %PREFIX%\bin
+copy scripts\post-link.sh %PREFIX%\bin\.%PKG_NAME%-post-link.sh
+copy scripts\pre-unlink.sh %PREFIX%\bin\.%PKG_NAME%-pre-unlink.sh
